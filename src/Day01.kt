903d43c095fc081b0921ece0e77874d066d835ed
Tolly Kulczycki <tolly.kulczycki@protonmail.com>
2022-12-01 19:06:33 +0100

    Complete Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,53 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var currentElf: Int = 0
+        val elfs = mutableListOf<Int>()
+
+        for(line in input)
+        {
+            if(line.isNotEmpty())
+            {
+                currentElf += line.toInt()
+            }else
+            {
+                elfs.add(currentElf)
+                currentElf = 0
+            }
+        }
+        elfs.add(currentElf)
+
+        return elfs.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var currentElf: Int = 0
+        val elfs = mutableListOf<Int>()
+
+        for(line in input)
+        {
+            if(line.isNotEmpty())
+            {
+                currentElf += line.toInt()
+            }else
+            {
+                elfs.add(currentElf)
+                currentElf = 0
+            }
+        }
+        elfs.add(currentElf)
+
+        elfs.sort()
+        elfs.reverse()
+
+        return elfs.take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("../inputs/Day01_test")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = readInput("../inputs/Day01")
     println(part1(input))
     println(part2(input))
 }
